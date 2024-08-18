1d0ddf40528077bf87f200eb6650087ef398afbd
Godfred <essiengodfred88@gmail.com>
2024-08-18 14:31:58 +0000

    added alx-react-app-props

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -2,4 +2,12 @@ import React from 'react';
 
 const UserContext = React.createContext();
 
+// function UserContext() {
+//     return 
+//     <>
+//         <h1>M</h1>
+//     </>;
+//   }
+  
+
 export default UserContext;
