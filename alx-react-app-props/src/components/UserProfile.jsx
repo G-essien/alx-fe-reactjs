b10b130d05bf351ee9620741d5aba1a293a553e8
Godfred <essiengodfred88@gmail.com>
2024-08-18 22:36:26 +0000

    do it again

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,16 @@
-import UserInfo from '../UserInfo';
+// src/UserDetails.jsx
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
-function UserProfile() {
-  return <UserInfo />;
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default UserProfile;
+export default UserDetails;
