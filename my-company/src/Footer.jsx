030ec937341d8877c5c0cb46df89b1aa21fb2edb
Godfred <essiengodfred88@gmail.com>
2024-08-18 16:02:50 +0000

    created my-company app

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', background: '#ddd', textAlign: 'center' }}>
+        <p>© 2024 Our Company. All Rights Reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
