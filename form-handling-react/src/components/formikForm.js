45845bf534e3697101d1b35aca8e4415b93b0a5d
Godfred <essiengodfred88@gmail.com>
2024-09-01 21:43:20 +0000

    create a form-handling-react

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,43 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email format').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+export const formikForm = () => (
+  <Formik
+    initialValues={{ username: '', email: '', password: '' }}
+    validationSchema={validationSchema}
+    onSubmit={(values, { setSubmitting }) => {
+      console.log(values);
+      setSubmitting(false);
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form>
+        <div>
+          <label>Username:</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="div" />
+        </div>
+        <div>
+          <label>Email:</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+        </div>
+        <button type="submit" disabled={isSubmitting}>
+          Register
+        </button>
+      </Form>
+    )}
+  </Formik>
+);
+
