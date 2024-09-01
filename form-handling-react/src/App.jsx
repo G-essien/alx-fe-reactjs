044fe6668aa7debc7259519799fb249ea565e688
Godfred <essiengodfred88@gmail.com>
2024-09-01 21:25:19 +0000

    create a form-handling-react project

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,13 @@
 import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/FormikForm';
+import FormikForm from './components/formikForm';
 
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>User Registration</h1>
-      <RegistrationForm />
       <FormikForm />
+      <RegistrationForm />
     </div>
   );
 }
