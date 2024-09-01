5446ee8a85efb66911d9be45fd48633bf422c845
Godfred <essiengodfred88@gmail.com>
2024-09-01 22:31:54 +0000

    create a react-router-advanced project

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,10 +2,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
-import ProfileDetails from './ProfileDetails';
-import ProfileSettings from './ProfileSettings';
-import BlogPost from './BlogPost';
-import ProtectedRoute from './ProtectedRoute';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
