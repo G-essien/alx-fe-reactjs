e324474784264a5ff44d09d1e8285c919913228c
Godfred <essiengodfred88@gmail.com>
2024-09-08 18:32:44 +0000

    Tailwind CSS setup

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ function UserProfile() {
         alt="User"
         className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4 text-center hover:text-blue-500 transition-colors duration-300">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-3 sm:my-4 text-center hover:text-blue-500 transition-colors duration-300">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600 text-center">
