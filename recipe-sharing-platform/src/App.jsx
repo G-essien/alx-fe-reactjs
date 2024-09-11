b6e99e6d1b6522dc2af78b3bd0348d03ded4ad40
Godfred <essiengodfred88@gmail.com>
2024-09-11 20:33:40 +0000

    added recipe-sharing-platform app

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,10 @@
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
     <div className="text-blue-500 text-center">
       <h1>Hello, Tailwind CSS is working!</h1>
+      <HomePage />
     </div>
   );
 }
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+/* In your tailwind.config.js, ensure that your purge paths are set */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // You can also enable dark mode if needed
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          500: '#34D399',
+          600: '#059669',
+        },
+      },
+    },
   },
   plugins: [],
 };
