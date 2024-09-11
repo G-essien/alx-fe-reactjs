97e308dcfeeab97ecda41af550b67f1d48c5ad60
Godfred <essiengodfred88@gmail.com>
2024-09-11 19:06:58 +0000

    added recipe-sharing-platform app

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-blue-500 text-center">
+      <h1>Hello, Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can also enable dark mode if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
