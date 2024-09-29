698317bf7e605d9142788204702af95ca9710cb2
Godfred <essiengodfred88@gmail.com>
2024-09-29 23:49:13 +0000

    Added github-user-serach

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css';  // Add this import statement to include Tailwind styles
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
